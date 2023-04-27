f298c54d011c2cdc3b05ec302a47c0257848e7a6
UBC Student <burtonm1@students.cs.ubc.ca>
2023-04-26 19:42:56 -0700

    add global play/pause/stop to sounds page

--- src/pages/player/soundcard.tsx
+++ src/pages/player/soundcard.tsx
@@ -7,25 +7,47 @@ import styles from '../../styles/Soundcard.module.scss';
 interface SoundCardProps {
   iconFile: any,
   soundFile: any;
+  globalPlayState: string;
+  setGlobalPlayState: any;
 }
 
 function SoundCard(props: SoundCardProps) {
   const [soundActive, setSoundActive] = React.useState(false)
+  const [soundStatePlay, setSoundStatePlay] = React.useState(false)
   const [volume, setVolume] = useState(.6);
-  const [play, { stop }] = useSound(props.soundFile, {volume, loop: true});
-
+  const [play, { stop, pause }] = useSound(props.soundFile, {volume, loop: true});
 
   let containerClassName = soundActive ? styles.soundcard__containerOn : styles.soundcard__containerOff;
 
+  React.useEffect(() => {
+    if (soundActive) {
+      if (props.globalPlayState === 'play') {
+        play();
+        setSoundStatePlay(true);
+      } else if (props.globalPlayState === 'pause') {
+        pause();
+        setSoundStatePlay(false);
+        props.setGlobalPlayState('');
+      } else if (props.globalPlayState === 'stop') {
+        stop();
+        setSoundActive(false);
+        setSoundStatePlay(false);
+        props.setGlobalPlayState('');
+      }
+    }
+  }, [props.globalPlayState]);
+
   const handleClick = React.useCallback(() => {
     if (soundActive) {
+      setSoundStatePlay(false);
       stop();
       setSoundActive(false);
     } else {
       play();
       setSoundActive(true);
+      setSoundStatePlay(true);
     }
-  }, [play, stop, soundActive]);
+  }, [play, stop, soundActive, props]);
 
   const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setVolume(parseFloat(event.target.value));
--- src/pages/player/sounds.tsx
+++ src/pages/player/sounds.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaPlay, FaPause, FaStop } from 'react-icons/fa';
 
 import style from '../../styles/Sounds.module.scss'
 import SoundCard from './soundcard';
 import { soundsData } from '../../data/soundsMeta';
 
 function Sounds() {
+  const [globalPlayState, setGlobalPlayState] = useState('');
+
+  const handlePlay = () => {
+    setGlobalPlayState('play');
+  };
+
+  const handlePause = () => {
+    setGlobalPlayState('pause');
+  };
+
+  const handleStop = () => {
+    setGlobalPlayState('stop');
+  };
+
   return (
     <>
-      <div className={style.sounds__container}>
-        {soundsData.map((sound, index) => (
-          <SoundCard key={index} soundFile={sound.sound} iconFile={sound.icon} />
-        ))}
+      <div className={style.sounds__soundsContainer}>
+        <div className={style.sounds__soundCardContainer}>
+          {soundsData.map((sound, index) => (
+            <SoundCard 
+                key={index} 
+                soundFile={sound.sound} 
+                iconFile={sound.icon} 
+                globalPlayState={globalPlayState}
+                setGlobalPlayState={setGlobalPlayState} />
+          ))}
+        </div>
+        <div className={style.sounds__controlButtons}>
+            <button onClick={handlePlay}><FaPlay /></button>
+            <button onClick={handlePause}><FaPause /></button>
+            <button onClick={handleStop}><FaStop /></button>
+        </div>
       </div>
     </>
   )
